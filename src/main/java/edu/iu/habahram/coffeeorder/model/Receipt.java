156dc3834fece5d22a3004e475309f341921fb58
daeaker <daeaker@iu.edu>
2024-02-20 21:13:55 -0500

    part 2

--- src/main/java/edu/iu/habahram/coffeeorder/model/Receipt.java
+++ src/main/java/edu/iu/habahram/coffeeorder/model/Receipt.java
@@ -1,4 +1,15 @@
 package edu.iu.habahram.coffeeorder.model;
 
 public record Receipt(String description, float cost) {
+    static int ID;
+
+    public static int getID() {
+        return ID;
+    }
+
+    public static void setID(int ID) {
+        Receipt.ID = ID;
+    }
+
+
 }
--- src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
+++ src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
@@ -3,13 +3,48 @@ package edu.iu.habahram.coffeeorder.repository;
 import edu.iu.habahram.coffeeorder.model.*;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+
 @Repository
 public class OrderRepository {
+
+
+    // This will track which order ID the repository is on
+    // It will be increased after each order
+    private int nextID = 1;
+
+    private static final String ORDER_DATABASE_NAME = "db.txt";
+    private static final String NEW_LINE = System.lineSeparator();
+
+    private static void appendToFile(Path path, String content) throws IOException {
+        Files.write(path,
+                content.getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND);
+    }
+
+
+
     public Receipt add(OrderData order) throws Exception {
         Beverage beverage = null;
         switch (order.beverage().toLowerCase()) {
             case "dark roast":
                 beverage = new DarkRoast();
+                break;
+            case "decaf":
+                beverage = new Decaf();
+                break;
+            case "espresso":
+                beverage = new Espresso();
+                break;
+            case "house blend":
+                beverage = new HouseBlend();
                 break;
         }
         if (beverage == null) {
@@ -23,11 +58,29 @@ public class OrderRepository {
                 case "mocha":
                     beverage = new Mocha(beverage);
                     break;
+                case "soy":
+                    beverage = new Soy(beverage);
+                    break;
+                case "whip":
+                    beverage = new Whip(beverage);
+                    break;
                 default:
                     throw new Exception("Condiment type '%s' is not valid".formatted(condiment));
             }
         }
         Receipt receipt = new Receipt(beverage.getDescription(), beverage.cost());
+        // Sets the ID for the receipt
+        // Increases the ID tracker
+        receipt.setID(this.nextID);
+        this.nextID++;
+
+        // Adds the order to the database
+        Path path = Paths.get(ORDER_DATABASE_NAME);
+        String data = receipt.getID() + ","+ receipt.cost() + "," + receipt.description();
+        appendToFile(path, data + NEW_LINE);
+
         return receipt;
     }
+
+
 }
